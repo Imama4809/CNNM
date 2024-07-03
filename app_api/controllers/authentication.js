3793b65e068edb16b8efe063703eec6183a2cc87
Imama48091 <imama.4809@gmail.com>
2024-07-03 18:34:36 -0400

    no dotenv

--- app_api/controllers/authentication.js
+++ app_api/controllers/authentication.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { access } = require('fs');
 const { verify } = require('crypto');
-require('dotenv').config();
+// require('dotenv').config();
 
 
 //figure this out 
